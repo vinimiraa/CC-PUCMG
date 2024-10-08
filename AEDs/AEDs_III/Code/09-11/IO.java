b207efcfb13b3f3db6ad1deccf86967d164ddc5c
Vinícius Miranda de Araújo <vinicius.araujo.1457564@sga.pucminas.br>
2024-10-07 21:26:54 -0300

    Update AEDs_III: Code

---
+++ AEDs/AEDs_III/Code/09-11/IO.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+
+public class IO {
+
+public static void main(String[] args) {
+
+    Scanner console;
+
+    try {
+        console = new Scanner(System.in);
+        int opcao;
+        do {
+
+            System.out.println("AEDsIII");
+            System.out.println("-------");
+            System.out.println("\n> Início");
+            System.out.println("1 - Categorias");
+            System.out.println("2 - Clientes");
+            System.out.println("0 - Sair");
+
+            System.out.print("Opção: ");
+            try {
+                opcao = Integer.valueOf(console.nextLine());
+            } catch(NumberFormatException e) {
+                opcao = -1;
+            }
+
+            switch (opcao) {
+                case 1:
+                    (new MenuCategorias()).menu();
+                    break;
+                case 2:
+                    // (new MenuClientes()).menu();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+
+        } while (opcao != 0);
+
+
+
+    } catch(Exception e) {
+        e.printStackTrace();
+    }
+
+}
+
+}
